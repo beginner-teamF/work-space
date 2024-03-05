bef9a5dc860e0034366502284dfabd5dc4968fea
ls-kasa <shimpei-kasamatsu@leapsec.co.jp>
2024-03-05 17:25:13 +0900

    不要なコメントアウトを削除

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-
-
-// // テトリスボードの作成
-// function createTetrisBoard(board) {
-//     //ボードサイズ
-//     const blockSize = 30;
-//     const boardRow = 20;
-//     const boardCol = 10;
-//     //キャンバスの取得
-//     const cvs = document.getElementById("cvs");
-//     const ctx=cvs.getContext("2d");
-//     //2dコンテキストを取得
-//     //キャンバスサイズ
-//     const canvasW = blockSize * boardCol;
-//     const canvasH = blockSize * boardRow;
-//     cvs.width = canvasW;
-//     cvs.height = canvasH;
-//     //コンテナの設定
-//     const container = document.getElementById("container");
-//     container.style.width = canvasW + 'px';
-//     //塗りに黒を設定
-//     ctx.fillStyle = '#000';
-//     //キャンバスを塗りつぶす
-//     ctx.fillRect(0, 0, canvasW, canvasH);
-// }
-
-// //初期化処理
+//初期化処理
 const init=()=>{
     new Tetris();
 }
@@ -97,31 +71,6 @@ class Tetris {
         const scoreElement = document.getElementById("score");
         scoreElement.textContent = `Score: ${this.score}`;
     }
-
-    // // テトリスブロックを制生成
-    // createTetrisBlock() {
-    //     // テトブロック一つ分
-    //     //T型のtet
-    //     let tet= [
-    //         [0, 0, 0, 0],
-    //         [0, 1, 0, 0],
-    //         [1, 1, 1, 0],
-    //         [0, 0, 0, 0],
-    //     ];
-
-    //     // ブロックの描画
-    //     for(let y = 0; y < this.tetSize; y++) {
-    //         for(let x = 0; x < this.tetSize; x++) {
-    //             if(tet[y][x]) {
-    //                 let px = this.offsetX + x;
-    //                 let py = this.offsetY + y;
-    //                 this.board[py][px] = 1;
-    //             }
-    //         }
-    //     }
-
-    //     this.draw();
-    // }
 
     //
     draw() {
